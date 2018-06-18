4363556952df4a7f98a29554891cac649438027c
Edvin CANDON <edvincandon@gmail.com>
2018-06-18 08:37:29 +0200

    Set up dev environment

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src'),
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'RxOfLife'
+		})
+	],
+
+	mode: 'production',
+
+	optimization: {
+		splitChunks: {
+			chunks: 'async',
+			minSize: 30000,
+			minChunks: 1,
+			name: false,
+
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: -10
+				}
+			}
+		}
+	}
+};
