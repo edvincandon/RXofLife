5425c7b3c2480c01c0839cb2b96ee7b7f7b8fbe1
Edvin CANDON <edvincandon@gmail.com>
2018-06-25 13:30:15 +0200

    Simplify

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
 
 
 import { interval, fromEvent, merge, animationFrameScheduler, NEVER, Subject } from 'rxjs';
-import { map, scan, mapTo, switchMap, startWith, shareReplay, withLatestFrom } from 'rxjs/operators';
+import { map, scan, mapTo, switchMap, startWith, shareReplay, share, withLatestFrom } from 'rxjs/operators';
 import { createGrid, nextGeneration, updateGrid, translateCells, getGridFromWindow } from './utils';
 import Renderer from './renderer';
 import { gliderGun } from './shapes';
@@ -22,38 +22,39 @@ const CELL_SIZE = 3;
 
 document.addEventListener('DOMContentLoaded', () => {
 	const renderer = new Renderer({ cellSize: CELL_SIZE });
-
 	const world$ = new Subject();
 	const spawn$ = new Subject();
 
 	const seed$ = fromEvent(SEED_BUTTON, 'click').pipe(mapTo(true));
 	const clear$ = fromEvent(CLEAR_BUTTON, 'click').pipe(mapTo(false));
-	const pause$ = fromEvent(PLAY_BUTTON, 'click').pipe(startWith(false), scan(x => !x));
+	const pause$ = fromEvent(PLAY_BUTTON, 'click').pipe(startWith(false), scan(x => !x), shareReplay(1));
+
 	const resize$ = fromEvent(window, 'resize').pipe(
 		startWith(null),
-		map(() => getGridFromWindow(CELL_SIZE)),
+		map(() => createGrid(getGridFromWindow(CELL_SIZE))),
 		shareReplay(1)
 	);
 
 	const click$ = fromEvent(renderer.world, 'click')
 		.pipe(
 			map(renderer.getCursorPosition),
-			withLatestFrom(spawn$, resize$),
-			map(([[x, y], { cells }, { columns, rows }]) =>
-				translateCells({ x, y, cells, columns, rows}))
+			withLatestFrom(spawn$, world$),
+			map(([[x, y], { cells }, { columns, rows, grid }]) =>
+				updateGrid({
+					grid,
+					cells: translateCells({ x, y, cells, columns, rows}),
+					columns,
+					rows
+				}))
 		);
 
-	const ticks$ = pause$
-		.pipe(switchMap(val => (val ? interval(0, animationFrameScheduler) : NEVER)), shareReplay(1));
-
-	// const ticks$ = merge(tick$.pipe(map(() => [])), click$)
-	// 	.pipe(shareReplay(1));
-
-	merge(seed$, clear$)
+	const reset$ = merge(seed$, clear$)
 		.pipe(
 			withLatestFrom(resize$),
-			map(([random, { columns, rows }]) => createGrid({ columns, rows, random })))
-		.subscribe(world$);
+			map(([random, { columns, rows }]) => createGrid({ columns, rows, random })));
+
+	const ticks$ = pause$
+		.pipe(switchMap(val => (val ? interval(0, animationFrameScheduler) : NEVER)));
 
 	const state$ = world$
 		.pipe(switchMap(({ grid: initGrid, columns, rows }) =>
@@ -62,18 +63,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 	/* SUBSCRIPTIONS */
-	state$.subscribe(({ updateMap }) => renderer.draw(updateMap));
-
+	/* draw initial frame and reinit renderer */
 	world$.subscribe(({ grid, columns, rows, clear }) => {
 		if (clear) renderer.init({ columns, rows });
 		renderer.draw(grid.filter(cell => cell[2] === 1));
 	});
+	/* redraw */
+	state$.subscribe(({ updateMap }) => renderer.draw(updateMap));
 
-	click$
-		.pipe(withLatestFrom(world$), map(([cells, { grid, columns, rows }]) => updateGrid({ grid, cells, columns, rows })))
-		.subscribe(world$);
-
-	resize$.pipe(map(createGrid)).subscribe(world$);
+	/* world source streams */
+	merge(click$, resize$, reset$).subscribe(world$);
 
 	pause$.subscribe(val => PLAY_BUTTON.innerHTML = val ? '❚❚' : '►');
 
