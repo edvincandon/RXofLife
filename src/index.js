c5d5e25ed33284966b10d14b36a58eb28e1980fd
Edvin CANDON <edvincandon@gmail.com>
2018-06-26 07:27:15 +0200

    lint

--- src/index.js
+++ src/index.js
@@ -23,71 +23,71 @@ const CELL_SIZE = 2;
 
 
 document.addEventListener('DOMContentLoaded', () => {
-	const renderer = new Renderer({ cellSize: CELL_SIZE });
+  const renderer = new Renderer({ cellSize: CELL_SIZE });
 
-	const world$ = new Subject();
-	const spawn$ = new Subject();
+  const world$ = new Subject();
+  const spawn$ = new Subject();
 
-	/* control streams */
-	const seed$ = fromEvent(SEED_BUTTON, 'click').pipe(mapTo(true));
-	const clear$ = fromEvent(CLEAR_BUTTON, 'click').pipe(mapTo(false));
-	const pause$ = fromEvent(PLAY_BUTTON, 'click').pipe(startWith(false), scan(x => !x), shareReplay(1));
+  /* control streams */
+  const seed$ = fromEvent(SEED_BUTTON, 'click').pipe(mapTo(true));
+  const clear$ = fromEvent(CLEAR_BUTTON, 'click').pipe(mapTo(false));
+  const pause$ = fromEvent(PLAY_BUTTON, 'click').pipe(startWith(false), scan(x => !x), shareReplay(1));
 
-	/* map each resize to a new grid
+  /* map each resize to a new grid
 	 * resize$ starts with null to trigger initial resize */
-	const resize$ = fromEvent(window, 'resize').pipe(
-		startWith(null),
-		map(() => createGrid(getGridFromWindow(CELL_SIZE))),
-		shareReplay(1)
-	);
+  const resize$ = fromEvent(window, 'resize').pipe(
+    startWith(null),
+    map(() => createGrid(getGridFromWindow(CELL_SIZE))),
+    shareReplay(1)
+  );
 
-	/* map each click to cells to update based on spawn type */
-	const click$ = fromEvent(renderer.world, 'click')
-		.pipe(
-			map(renderer.getCursorPosition),
-			withLatestFrom(spawn$, world$),
-			map(([[x, y], { cells }, { columns, rows, grid }]) =>
-				updateGrid({
-					grid,
-					cells: translateCells({ x, y, cells, columns, rows}),
-					columns,
-					rows
-				}))
-		);
+  /* map each click to cells to update based on spawn type */
+  const click$ = fromEvent(renderer.world, 'click')
+    .pipe(
+      map(renderer.getCursorPosition),
+      withLatestFrom(spawn$, world$),
+      map(([[x, y], { cells }, { columns, rows, grid }]) =>
+        updateGrid({
+          grid,
+          cells: translateCells({ x, y, cells, columns, rows}),
+          columns,
+          rows
+        }))
+    );
 
-	/* handle grid resets via seed or clear button */
-	const reset$ = merge(seed$, clear$)
-		.pipe(
-			withLatestFrom(resize$),
-			map(([random, { columns, rows }]) => createGrid({ columns, rows, random })));
+  /* handle grid resets via seed or clear button */
+  const reset$ = merge(seed$, clear$)
+    .pipe(
+      withLatestFrom(resize$),
+      map(([random, { columns, rows }]) => createGrid({ columns, rows, random })));
 
-	/* main game ticker */
-	const ticks$ = pause$
-		.pipe(switchMap(val => (val ? interval(50, animationFrameScheduler) : NEVER)));
+  /* main game ticker */
+  const ticks$ = pause$
+    .pipe(switchMap(val => (val ? interval(50, animationFrameScheduler) : NEVER)));
 
-	/* main game loop */
-	const state$ = world$
-		.pipe(switchMap(({ grid: initGrid, columns, rows }) =>
-			ticks$.pipe(scan(({ grid }) => nextGeneration({ grid, columns, rows }),
-				{ grid: initGrid }))));
+  /* main game loop */
+  const state$ = world$
+    .pipe(switchMap(({ grid: initGrid, columns, rows }) =>
+      ticks$.pipe(scan(({ grid }) => nextGeneration({ grid, columns, rows }),
+        { grid: initGrid }))));
 
 
-	/* SUBSCRIPTIONS */
-	/* draw initial frame and reinit renderer if needed */
-	world$.subscribe(({ grid, columns, rows, clear }) => {
-		if (clear) renderer.init({ columns, rows });
-		renderer.draw(grid.filter(cell => cell[2] === 1));
-	});
-	/* redraw */
-	state$.subscribe(({ updateMap }) => renderer.draw(updateMap));
+  /* SUBSCRIPTIONS */
+  /* draw initial frame and reinit renderer if needed */
+  world$.subscribe(({ grid, columns, rows, clear }) => {
+    if (clear) renderer.init({ columns, rows });
+    renderer.draw(grid.filter(cell => cell[2] === 1));
+  });
+  /* redraw */
+  state$.subscribe(({ updateMap }) => renderer.draw(updateMap));
 
-	/* merge source streams */
-	merge(click$, resize$, reset$).subscribe(world$);
+  /* merge source streams */
+  merge(click$, resize$, reset$).subscribe(world$);
 
-	pause$.subscribe(val => PLAY_BUTTON.innerHTML = val ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>');
+  pause$.subscribe(val => PLAY_BUTTON.innerHTML = val ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>');
 
-	spawn$.next({
-		type: 'single',
-		cells: gliderGun
-	});
+  spawn$.next({
+    type: 'single',
+    cells: gliderGun
+  });
 });
--- src/renderer.js
+++ src/renderer.js
@@ -3,41 +3,41 @@ const COLOR_ALIVE = '#71B48D';
 const COLOR_DIRTY = '#251F47';
 
 export default class Renderer {
-	constructor({ cellSize }) {
-		this.world = document.getElementById('world');
-		this.ctx = this.world.getContext('2d');
-		this.cellSize = cellSize * window.devicePixelRatio;
-		this.getCursorPosition = this.getCursorPosition.bind(this);
-		this.init({ rows: 0, columns: 0 });
-	}
+  constructor({ cellSize }) {
+    this.world = document.getElementById('world');
+    this.ctx = this.world.getContext('2d');
+    this.cellSize = cellSize * window.devicePixelRatio;
+    this.getCursorPosition = this.getCursorPosition.bind(this);
+    this.init({ rows: 0, columns: 0 });
+  }
 
-	init({ rows, columns }) {
-		this.width = columns;
-		this.height = rows;
-		const { cellSize, world, height, width, ctx } = this;
+  init({ rows, columns }) {
+    this.width = columns;
+    this.height = rows;
+    const { cellSize, world, height, width, ctx } = this;
 
-		world.style.height = height * cellSize + 'px';
-		world.style.width = width * cellSize + 'px';
-		world.setAttribute('height', height * cellSize);
-		world.setAttribute('width', width * cellSize);
-		ctx.fillStyle = COLOR_DEAD;
-		ctx.fillRect(0, 0, width * cellSize, height * cellSize);
-	}
+    world.style.height = height * cellSize + 'px';
+    world.style.width = width * cellSize + 'px';
+    world.setAttribute('height', height * cellSize);
+    world.setAttribute('width', width * cellSize);
+    ctx.fillStyle = COLOR_DEAD;
+    ctx.fillRect(0, 0, width * cellSize, height * cellSize);
+  }
 
-	draw(updateMap) {
-		const { cellSize, ctx } = this;
-		updateMap.forEach(([x, y, status]) => {
-			ctx.fillStyle = status === 1 ? COLOR_ALIVE : COLOR_DIRTY;
-			/* account for border */
-			ctx.fillRect(x * cellSize + 1, y * cellSize + 1, cellSize - 1 , cellSize - 1);
-		});
-	}
+  draw(updateMap) {
+    const { cellSize, ctx } = this;
+    updateMap.forEach(([x, y, status]) => {
+      ctx.fillStyle = status === 1 ? COLOR_ALIVE : COLOR_DIRTY;
+      /* account for border */
+      ctx.fillRect(x * cellSize + 1, y * cellSize + 1, cellSize - 1 , cellSize - 1);
+    });
+  }
 
-	getCursorPosition(event) {
-		const { world, cellSize } = this;
-		const rect = world.getBoundingClientRect();
-		const x = Math.floor((event.clientX - rect.left) / cellSize);
-		const y = Math.floor((event.clientY - rect.top) / cellSize);
-		return [x, y];
-	}
+  getCursorPosition(event) {
+    const { world, cellSize } = this;
+    const rect = world.getBoundingClientRect();
+    const x = Math.floor((event.clientX - rect.left) / cellSize);
+    const y = Math.floor((event.clientY - rect.top) / cellSize);
+    return [x, y];
+  }
 }
