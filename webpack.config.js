54961280aa6429dd561d3dd247e56ef8c034d647
Edvin CANDON <edvincandon@gmail.com>
2020-04-05 11:38:58 +0200

    clean-up and update build tooling

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 module.exports = {
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
 				include: path.resolve(__dirname, 'src'),
-				loader: 'babel-loader',
-				exclude: /node_modules/
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
 			}
 		]
 	},
-
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'RxOfLife',
 			template: path.resolve(__dirname, 'static') + '/index.html'
 		})
 	],
-
-	mode: 'production',
-
-	optimization: {
-		splitChunks: {
-			chunks: 'async',
-			minSize: 30000,
-			minChunks: 1,
-			name: false,
-
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10
-				}
-			}
-		}
-	}
+	mode: 'production'
 };
